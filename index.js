c7684f22312004bcb16e69d6d5b84818fdb73aa4
WaleriiChirkov-16021990 <zx84058405@live.com>
2023-12-15 23:21:53 +0300

    final homework

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-// const express = require('express');
-// const app = express();
-// const port = 3000;
-//
-// app.get('/',(req,res)=>{
-//     app.use(express.static('public'));
-//     app.sendFile('mainPage.html');
-// });
-//
-// app.get('/about',(req,res)=>{
-//     app.use(express.static('public'));
-//     app.sendFile('about.html');
-// });
-//
-// app.listen(port,()=>{
-//     console.log(`listening on port ${port}`);
-// })
+'use strict';
 
 const express = require('express');
 const fs = require('fs');
@@ -23,34 +7,27 @@ const path = require('path');
 const app = express();
 
 // Helper function to update and save the counter value to a file
-function updateCounter() {
-    let counter = 0;
-
-    return function() {
-        counter++;
-        fs.writeFile('counter.txt', counter.toString(), (err) => {
-            if (err) throw err;
-        });
-    };
+const updateCounterValue = function updateCounter(data) {
+    fs.writeFileSync(path.join(__dirname, 'public', 'data.json'), JSON.stringify(data));
 }
 
 // Load the counter value from the file when the server starts
 
-let counterValue = parseInt(fs.readFileSync('counter.txt'));
+const counterValue = JSON.parse(fs.readFileSync('public/data.json').toString());
 
-// Create the counter update function
-const updateCounterValue = updateCounter();
 
 // Handler for the home page ("/")
 app.get('/', (req, res) => {
-    updateCounterValue();
-    res.send(`Home Page<br>Views: ${counterValue}<br><a href="/about">About</a>`);
+    counterValue.main = counterValue.main + 1;
+    updateCounterValue(counterValue);
+    res.send(`Home Page<br>Views: ${counterValue.main}<br><a href="/about">About</a>`);
 });
 
 // Handler for the about page ("/about")
 app.get('/about', (req, res) => {
-    updateCounterValue();
-    res.send(`About Page<br>Views: ${counterValue}<br><a href="/">Main Page</a>`);
+    counterValue.about = counterValue.about + 1;
+    updateCounterValue(counterValue)
+    res.send(`About Page<br>Views: ${counterValue.about}<br><a href="/">Main Page</a>`);
 });
 
 // Start the server
